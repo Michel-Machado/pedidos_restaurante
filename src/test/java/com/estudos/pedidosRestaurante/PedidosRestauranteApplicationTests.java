c5e26593fa74c819875dd71228a4a08ee2edb275
Michel Machado <michelmachado125@gmail.com>
2023-11-10 22:14:14 -0300

    first commit

---
+++ src/test/java/com/estudos/pedidosRestaurante/PedidosRestauranteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudos.pedidosRestaurante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedidosRestauranteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
